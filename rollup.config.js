32cbf178ee3510670e56c8e64ea1b4c9c07e9cc9
mcolo <max.miracolo@gmail.com>
2022-07-29 12:51:11 -0400

    revert

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import { terser } from 'rollup-plugin-terser';
 export default {
   input: 'music-for-business/scripts/main.js',
   output: { 
-    file: 'public/assets/mfb-main.js'
+    dir: 'public/music-for-business/'
   },
   plugins: [
     terser()
